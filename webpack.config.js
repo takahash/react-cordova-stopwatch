96ac13b1b5586fb9ed5d361d9da7373ed3a4d8a2
Ken Takahashi <takaken@redimpulz.com>
2016-12-22 15:43:05 +0900

    出力先を修正

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/index.jsx'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'bundle'),
     filename: 'bundle.js',
     publicPath: '/build/'
   },
